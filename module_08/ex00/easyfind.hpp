1848801366c9a83ec6b421fb76c5b0ccf6864d1f
cclaude42 <cesarclaudepro@gmail.com>
2020-10-18 17:57:24 +0200

    testing easyfind

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -6,20 +6,22 @@
 /*   By: anonymou <anonymou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 17:32:22 by anonymou          #+#    #+#             */
-/*   Updated: 2020/10/18 17:46:33 by anonymous        ###   ########.fr       */
+/*   Updated: 2020/10/18 17:57:08 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-template <typename T>
+template <typename T, typename S>
 int	easyfind(T & array, int n)
 {
-	typename T::iterator	loc;
+	size_t						size = sizeof(array) / sizeof(array[0]);
+	std::vector<S>				vec(array, array + size);
+	std::vector<S>::iterator	pos;
 
-	loc = std::find(std::begin(array), std::end(array), n);
+	pos = std::find(vec.begin, vec.end, n);
 
-	if (loc == std::end(array))
+	if (pos == vec.end)
 		throw "Can't found element in container !";
 
 	return (*loc);
