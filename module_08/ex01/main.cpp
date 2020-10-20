0bb5131dd78efbc2737f3a3635d3f28e4b0405d8
cclaude42 <cesarclaudepro@gmail.com>
2020-10-20 20:39:05 +0200

    08 span

--- module_08/ex01/main.cpp
+++ module_08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anonymou <anonymou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 20:01:45 by anonymou          #+#    #+#             */
-/*   Updated: 2020/10/20 19:48:56 by anonymous        ###   ########.fr       */
+/*   Updated: 2020/10/20 20:38:52 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,37 @@
 
 int	main(void)
 {
-	Storage		stor(100000);
+	Storage		stor(100004);
 
-	for (int i = 1 ; i <= 100001 ; i++)
-	{
-		try
-		{
-			stor.addNumber(i);
-		}
-		catch (std::exception & e)
-		{
-			std::cout << "Exception : " << e.what() << std::endl;
-		}
-	}
+	for (int i = 1 ; i <= 100000 ; i++)
+		stor.addNumber(i * 2);
 
-	std::cout << stor;
+	// Shortest should be 2
+	// Longest should be 199998
+
 	std::cout << "Shortest span : " << stor.shortestSpan() << std::endl;
 	std::cout << "Longest span : " << stor.longestSpan() << std::endl;
+
+	stor.addNumber(45673); // Make shortest span 1
+	stor.addNumber(1000000); // Make longest span 999998
+
+	std::cout << "Shortest span : " << stor.shortestSpan() << std::endl;
+	std::cout << "Longest span : " << stor.longestSpan() << std::endl;
+
+	stor.addNumber(57823); // Make shortest span 0
+	stor.addNumber(-1000000); // Make longest span 2000000
+
+	std::cout << "Shortest span : " << stor.shortestSpan() << std::endl;
+	std::cout << "Longest span : " << stor.longestSpan() << std::endl;
+
+	try
+	{
+		stor.addNumber(100);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << "Exception : " << e.what() << std::endl;
+	}
 
 	return (0);
 }
