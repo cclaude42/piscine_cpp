078a413d5db1af66062ead0e64d52c99a709412f
cclaude42 <cesarclaudepro@gmail.com>
2020-10-21 19:59:03 +0200

    08/02

--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -6,11 +6,12 @@
 /*   By: anonymou <anonymou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 17:13:59 by anonymou          #+#    #+#             */
-/*   Updated: 2020/10/21 19:51:20 by anonymous        ###   ########.fr       */
+/*   Updated: 2020/10/21 19:58:57 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <iomanip>
 #include <list>
 
 int		main(void)
@@ -19,14 +20,14 @@ int		main(void)
 	std::list<int>		lst;
 
 	std::cout << " Mutant Stack |   std::list  " << std::endl;
-	std::cout << "-----------------------------"
+	std::cout << "-----------------------------" << std::endl;
 
 	mstack.push(5);
-	lst.push(5);
+	lst.push_back(5);
 	mstack.push(17);
-	lst.push(17);
+	lst.push_back(17);
 
-	std::cout << "Top :" << std:endl;
+	std::cout << "Top :" << std::endl;
 	std::cout << std::setw(13) << mstack.top() << " |" << std::setw(13) << lst.top() << std::endl;
 
 	mstack.pop();
@@ -36,13 +37,13 @@ int		main(void)
 	std::cout << std::setw(13) << mstack.size() << " |" << std::setw(13) << lst.size() << std::endl;
 
 	mstack.push(3);
-	lst.push(3);
+	lst.push_back(3);
 	mstack.push(5);
-	lst.push(5);
+	lst.push_back(5);
 	mstack.push(737);
-	lst.push(737);
+	lst.push_back(737);
 	mstack.push(0);
-	lst.push(0);
+	lst.push_back(0);
 
 	std::cout << "Top :" << std::endl;
 	std::cout << std::setw(13) << mstack.top() << " |" << std::setw(13) << lst.top() << std::endl;
@@ -50,7 +51,7 @@ int		main(void)
 	MutantStack<int>::iterator	mit = mstack.begin();
 	std::list<int>::iterator	lit = lst.begin();
 	MutantStack<int>::iterator	mend = mstack.end();
-	std::list<int>::iterator	lend = mstack.end();
+	std::list<int>::iterator	lend = lst.end();
 
 	++mit;
 	++lit;
@@ -71,18 +72,15 @@ int		main(void)
 	std::cout << "Top :" << std::endl;
 	std::cout << std::setw(13) << s.top() << " |" << std::setw(13) << l.top() << std::endl;
 
-	MutantStack<int>	mstack2;
-	std::list<int>		lst2;
-
-	mstack2 = mstack;
-	lst2 = lst;
+	MutantStack<int>	mstack2 = mstack;
+	std::list<int>		lst2 = lst;
 
 	mstack2.push(2);
-	lst2.push(2);
+	lst2.push_back(2);
 	mstack2.push(8);
-	lst2.push(8);
+	lst2.push_back(8);
 	mstack2.push(12);
-	lst2.push(12);
+	lst2.push_back(12);
 
 	mit = mstack2.begin();
 	lit = lst2.begin();
