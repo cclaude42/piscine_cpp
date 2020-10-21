d8478f749fa460e9117c3522111a04bb8ddea451
cclaude42 <cesarclaudepro@gmail.com>
2020-10-21 18:10:54 +0200

    08/02

--- module_08/ex02/MutantStack.cpp
+++ module_08/ex02/MutantStack.cpp
@@ -6,25 +6,11 @@
 /*   By: anonymou <anonymou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 17:34:39 by anonymou          #+#    #+#             */
-/*   Updated: 2020/10/21 18:06:07 by anonymous        ###   ########.fr       */
+/*   Updated: 2020/10/21 18:10:46 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
-
-// Member functions
-
-template <typename T>
-iterator	MutantStack::begin(void)
-{
-	return (std::begin(this->c));
-}
-
-template <typename T>
-iterator	MutantStack::end(void)
-{
-	return (std::end(this->c));
-}
 
 // Overloaders
 
--- module_08/ex02/mutantstack.hpp
+++ module_08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: anonymou <anonymou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 15:36:49 by anonymou          #+#    #+#             */
-/*   Updated: 2020/10/21 17:54:41 by anonymous        ###   ########.fr       */
+/*   Updated: 2020/10/21 18:10:28 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ public:
 
 	MutantStack	&operator=(const MutantStack & src);
 
-	iterator	begin(void);
-	iterator	end(void);
+	iterator	begin(void) { return (std::begin(this->c)); };
+	iterator	end(void) { return (std::end(this->c)); };
 };
 
 #endif
