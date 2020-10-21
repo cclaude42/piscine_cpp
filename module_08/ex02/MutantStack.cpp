0071fe2a19c252218c3e5a5241fc221f387d6b8b
cclaude42 <cesarclaudepro@gmail.com>
2020-10-21 20:19:36 +0200

    08/02

--- module_08/ex02/MutantStack.cpp
+++ module_08/ex02/MutantStack.cpp
@@ -6,7 +6,7 @@
 /*   By: anonymou <anonymou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 19:27:05 by anonymou          #+#    #+#             */
-/*   Updated: 2020/10/21 20:07:10 by anonymous        ###   ########.fr       */
+/*   Updated: 2020/10/21 20:19:29 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 // Member functions
 
 template <typename T>
-MutantStack<T>::iterator		MutantStack<T>::begin(void)
+typename MutantStack<T>::iterator		MutantStack<T>::begin(void)
 {
 	return (this->c.begin());
 }
 
 template <typename T>
-MutantStack<T>::iterator		MutantStack<T>::end(void)
+typename MutantStack<T>::iterator		MutantStack<T>::end(void)
 {
 	return (this->c.end());
 }
